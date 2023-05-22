cf71c76bc140795709314f3dd4fcd01ae9098f17
owonikokomide <donaykoko@yahho.com>
2023-05-22 15:45:26 +0100

    update

---
+++ sanwo.js
@@ -0,0 +1,28 @@
+// function homeover(){
+//   document.getElementById("home").style.backgroundColor = "#ccd9ff";
+// }
+// function homeout(){
+//   document.getElementById("home").style.backgroundColor = "";
+// }
+// function savingsover(){
+//   document.getElementById("savings").style.backgroundColor = "#ccd9ff";
+// }
+// function savingsout(){
+//   document.getElementById("savings").style.backgroundColor = "";
+// }
+// function invover(){
+//   document.getElementById("investment").style.backgroundColor = "#ccd9ff";
+// }
+// function invout(){
+//   document.getElementById("investment").style.backgroundColor = "";
+// }
+
+
+
+
+
+
+
+// function accover(){
+//   document.getElementById("account").style.backgroundColor = "green";
+// }
